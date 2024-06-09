192460976c518f3436724378fbf64a39d3be3bc9
TorkRobotics <torkrobotics@gmail.com>
2024-06-08 23:15:54 -0600

    Base setup: Window initialization.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,8 @@
+import game.Window;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Window window = Window.get();
+        window.run();
     }
 }
--- src/main/java/game/Window.java
+++ src/main/java/game/Window.java
@@ -1,4 +1,76 @@
 package game;
 
+import org.lwjgl.glfw.GLFWErrorCallback;
+import org.lwjgl.opengl.GL;
+
+import static org.lwjgl.glfw.Callbacks.glfwFreeCallbacks;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.NULL;
+
 public class Window {
+    private int width, height;
+    private String title;
+    private long glfwWindow;
+    public float r,g,b,a;
+    public static Window window = null;
+
+    private Window() {
+        this.width  = 1920;
+        this.height = 1080;
+        this.title = "Tetris by ACJ";
+
+        this.r = 1.0f;
+        this.g = 1.0f;
+        this.b = 1.0f;
+        this.a = 1.0f;
+    }
+
+    public static Window get() {
+        if(Window.window == null) {
+            Window.window = new Window();
+        }
+
+        return Window.window;
+    }
+
+    public void run(){
+        init();
+        loop();
+
+        glfwFreeCallbacks(glfwWindow);
+        glfwDestroyWindow(glfwWindow);
+    }
+    public void init() {
+        GLFWErrorCallback.createPrint(System.err).set();
+        if(!glfwInit()) {
+            throw  new IllegalStateException("Unable to initialize GLFW.");
+        }
+
+        glfwDefaultWindowHints();
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_MAXIMIZED, GLFW_TRUE);
+
+        glfwWindow = glfwCreateWindow(this.width, this.height, this.title, NULL, NULL);
+        if(glfwWindow == NULL) {
+            throw new IllegalStateException("Failed to create GLFW window.");
+        }
+
+        glfwMakeContextCurrent(glfwWindow);
+        glfwSwapInterval(1);
+
+        glfwShowWindow(glfwWindow);
+
+        GL.createCapabilities();
+
+    }
+    private void loop() {
+        while(!glfwWindowShouldClose(glfwWindow)) {
+            glfwPollEvents();
+            glClearColor(r, g, b, a);
+            glClear(GL_COLOR_BUFFER_BIT);
+            glfwSwapBuffers(glfwWindow);
+        }
+    }
 }
