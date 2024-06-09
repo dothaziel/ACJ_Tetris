b4d353f2265e4dc85ede07d3f35356f9aa9d83c7
Haziel Campos <169002063+dothaziel@users.noreply.github.com>
2024-06-09 00:10:15 -0600

    Base setup: Scene manager and KeyListeners
    - KeyListener Events
    - MouseListener Events
    - Scene Manager
    - DeltaTime added

---
+++ src/main/java/game/scenes/Menu.java
@@ -0,0 +1,14 @@
+package game.scenes;
+
+import game.Scene;
+
+public class Menu extends Scene {
+
+    public Menu() {
+
+    }
+    @Override
+    public void update(float deltaTime) {
+
+    }
+}
